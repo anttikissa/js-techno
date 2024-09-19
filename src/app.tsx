b6c645d8b63c4431bc1b2bdf1ecd096123e3389a
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2024-09-19 16:25:18 +0300

    Correct wording

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import Techno from '~/components/Techno'
 export default function App() {
 	return (
 		<main>
-			<h1>Press play for some noise!</h1>
+			<h1>Press play for some techno!</h1>
 			<Techno />
 		</main>
 	)
